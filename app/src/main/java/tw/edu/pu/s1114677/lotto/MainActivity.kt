622ce9b48dcb72e48674201f9710c01bc65409ca
LLLTZUYIN <b203120708@gmail.com>
2024-03-06 17:59:57 +0800

    第二次 Commit(加入樂透功能)

--- app/src/main/java/tw/edu/pu/s1114677/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114677/lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.s1114677.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var t: TextView = findViewById(R.id.txv)
-        t.text = "app"
+        t.text = "0"
     }
+
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn){
+            txv.text = (1..100).random().toString()
+        }
+        else{
+            txv.text = "0"
+        }
+    }
+
+
 }
